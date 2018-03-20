9828997355dd01625d6dbdfcf141edf4fc809c60
Jared Hoberock <jaredhoberock@gmail.com>
2018-03-20 18:49:23 -0500

    Add some more notes, receive ExecutionPolicy via forwarding reference

--- for_each.cpp
+++ for_each.cpp
@@ -7,10 +7,11 @@
 #include "acc.hpp"
 
 
-// to compose with Thrust, we need to define an execution policy type
-// derive from agency::basic_execution_policy to get conveniences like .on(), .executor(), etc.
-// derive from thrust::execution_policy to hook into Thrust's algorithm dispatch machinery
-// XXX it would be nice not to have to derive from thrust::execution_policy and instead simply define overloads for the algorithms we're interested in customizing, as we do below
+// to compose with Thrust, we need to define an execution policy type:
+//
+// 1. derive from agency::basic_execution_policy to get conveniences like .on(), .executor(), etc. Thrust is oblivious to these conveniences.
+// 2. derive from thrust::execution_policy to hook into Thrust's algorithm dispatch machinery. This is the part Thrust cares about.
+//    XXX it would be nice not to have to derive from thrust::execution_policy and instead simply define overloads for the algorithms we're interested in customizing, as we do below
 template<class Executor>
 class basic_parallel_policy : public agency::basic_execution_policy<agency::parallel_agent, Executor, basic_parallel_policy<Executor>>,
                               public thrust::execution_policy<basic_parallel_policy<Executor>>
@@ -33,7 +34,7 @@ using ignore_t = decltype(std::ignore);
 
 
 template<class ExecutionPolicy, class Iterator, class Function>
-Iterator for_each(ExecutionPolicy policy, Iterator first, Iterator last, Function f)
+Iterator for_each(ExecutionPolicy&& policy, Iterator first, Iterator last, Function f)
 {
   std::cout << "Hello, world from for_each(my_policy)!" << std::endl;
 
@@ -77,8 +78,10 @@ int main()
 
   basic_parallel_policy<agency::parallel_executor> par;
   
+  // when we call thrust::for_each with our custom execution policy, it will find the overload of for_each above
   thrust::for_each(par.on(acc_executor()), vec.begin(), vec.end(), thrust::identity<int>());
 
+  // when we call thrust::transform with our custom execution policy, it will implement the transform via our overload of for_each above
   thrust::transform(par.on(acc_executor()), vec.begin(), vec.end(), vec.begin(), thrust::negate<int>());
 
   assert(vec[0] == -13);
