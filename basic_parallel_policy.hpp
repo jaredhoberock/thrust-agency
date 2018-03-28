52c48b30639db3cc661db103a976c853cb5a6653
Jared Hoberock <jaredhoberock@gmail.com>
2018-03-28 15:46:19 -0500

    Derive from thrust::execution_policy

--- basic_parallel_policy.hpp
+++ basic_parallel_policy.hpp
@@ -27,9 +27,16 @@
 #pragma once
 
 #include <agency/agency.hpp>
+#include <thrust/execution_policy.h>
 
+// to compose with Thrust, we need to define an execution policy type:
+//
+// 1. derive from agency::basic_execution_policy to get conveniences like .on(), .executor(), etc. Thrust is oblivious to these conveniences.
+// 2. derive from thrust::execution_policy to hook into Thrust's algorithm dispatch machinery. This is the part Thrust cares about.
+//    XXX it would be nice not to have to derive from thrust::execution_policy and instead simply define overloads for the algorithms we're interested in customizing, as we do below
 template<class Executor>
-class basic_parallel_policy : public agency::basic_execution_policy<agency::parallel_agent, Executor, basic_parallel_policy<Executor>>
+class basic_parallel_policy : public agency::basic_execution_policy<agency::parallel_agent, Executor, basic_parallel_policy<Executor>>,
+                              public thrust::execution_policy<basic_parallel_policy<Executor>>
 {
   private:
     using super_t = agency::basic_execution_policy<agency::parallel_agent, Executor, basic_parallel_policy<Executor>>;
